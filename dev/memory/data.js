47928c453f18fe264924fbfc8b2e66c25fc82773
github-action-benchmark <github@users.noreply.github.com>
2026-07-20 15:02:04 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 10b9d2e062834abef4f790249bd5178e5aaab874

---
+++ dev/memory/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1784559724741,
+  "repoUrl": "https://github.com/gcavanunez/phpantom_lsp",
+  "entries": {
+    "PHPantom Memory Usage": [
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "10b9d2e062834abef4f790249bd5178e5aaab874",
+          "message": "Add task for more Mago migration",
+          "timestamp": "2026-07-20T08:24:36+02:00",
+          "tree_id": "e037a2afe8ee007717a7ab3dced5669829665812",
+          "url": "https://github.com/gcavanunez/phpantom_lsp/commit/10b9d2e062834abef4f790249bd5178e5aaab874"
+        },
+        "date": 1784559724286,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 50.5,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 72.7,
+            "unit": "MiB"
+          }
+        ]
+      }
+    ]
+  }
+}
